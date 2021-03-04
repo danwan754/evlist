c3e074dab4463b26339aa46e9409eb8499bcf6bd
danwan754 <danwan754@gmail.com>
2021-03-04 00:58:37 -0800

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,27 @@ import HomeScreen from './screens/HomeScreen';
 import RankingScreen from './screens/RankingScreen';
 import CompareScreen from './screens/CompareScreen';
 import ListingScreen from './screens/ListingScreen';
-import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header>
-          <div className="brand">
+          <div className="header-brand">
             <Link to="/">EV-List</Link>
           </div>
           <div className="header-links">
-            <Link to="/rankings">Rankings</Link>
-            <Link to="/compare">Compare</Link>
-            <Link to="/listing/tesla">Tesla</Link>
-            <Link to="/listing/generalmotors">General Motors</Link>
-            <Link to="/listing/nissan">Nissan</Link>
-            <Link to="/listing/porsche">Porsche</Link>
-            <Link to="/listing/other">Other</Link>
+            <div className="header-links-functions">
+              <Link to="/rankings">Rankings</Link>
+              <Link to="/compare">Compare</Link>
+            </div>
+            <div className="header-links-manufacturers">
+              <Link to="/listing/tesla">Tesla</Link>
+              <Link to="/listing/generalmotors">General Motors</Link>
+              <Link to="/listing/nissan">Nissan</Link>
+              <Link to="/listing/porsche">Porsche</Link>
+              <Link to="/listing/other">Other</Link>
+            </div>
           </div>
         </header>
         <main>
