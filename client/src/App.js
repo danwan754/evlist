29beeab82658c6420827fbb2e0428a04c1c80433
danwan754 <danwan754@gmail.com>
2021-03-06 22:24:36 -0800

    Fix unintended wrap in nav

--- client/src/App.js
+++ client/src/App.js
@@ -13,18 +13,16 @@ function App() {
           <div className="header-brand">
             <Link to="/">EV-List</Link>
           </div>
-          <div className="header-links">
-            <div className="header-links-functions">
+          <div className="header-links-functions">
               <Link to="/rankings">Rankings</Link>
               <Link to="/compare">Compare</Link>
-            </div>
-            <div className="header-links-manufacturers">
+          </div>
+          <div className="header-links-manufacturers">
               <Link to="/listing/tesla">Tesla</Link>
-              <Link to="/listing/generalmotors">General Motors</Link>
+              <Link to="/listing/generalmotors">GM</Link>
               <Link to="/listing/nissan">Nissan</Link>
               <Link to="/listing/porsche">Porsche</Link>
               <Link to="/listing/other">Other</Link>
-            </div>
           </div>
         </header>
         <main>
