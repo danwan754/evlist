dda350328fe261337f7220ec124fa262e73a3bc6
danwan754 <danwan754@gmail.com>
2021-03-30 18:50:53 -0700

    Move server code out of server folder for Heroku deploy

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from 'express';
-
+import path from 'path';
 import carRoute from './routes/carRoute.js';
 
+const __dirname = process.cwd();
 var app = express();
 app.use(express.json());
 
@@ -10,10 +11,10 @@ app.use('/api', carRoute);
 // access vehicle images
 app.use('/images', express.static('data/images'));
 
-// app.use(express.static(path.join(__dirname, '/../client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
-// });
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
