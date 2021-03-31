f72b306c8e048f8dfa4432bfb31465f1ff5187cf
danwan754 <danwan754@gmail.com>
2021-03-30 19:12:02 -0700

    Fix server to listen to heroku's port

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.listen(5000, () => {
-    console.log('Server started at http://localhost:5000');
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server started https://ev-list.herokuapp.com');
 });
